21225b250323b87c1b14925729a10099f972784e
BRZeph <148656729+BRZeph@users.noreply.github.com>
2025-04-22 11:53:07 -0300

    init
    git init

---
+++ src/test/java/brzeph/spring/java_spring_demo/JavaSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package brzeph.spring.java_spring_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
